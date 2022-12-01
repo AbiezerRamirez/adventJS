4c7f0d5d72682d4a5a95423f1a35fef83d539336
AbiezerRamirez <abiezer254@gmail.com>
2022-12-01 10:24:32 +0100

    ✨ Challenge 22

---
+++ reto22.js
@@ -0,0 +1,52 @@
+const tree = {
+    value: null, // el nodo raíz siempre es uno, porque es la estrella ⭐
+    left: {
+        value: 2, // el nodo izquierdo necesita dos decoraciones
+        left: null, // no tiene más ramas
+        right: null, // no tiene más ramas
+    },
+    right: {
+        value: 3, // el nodo de la derecha necesita tres decoraciones
+        left: null, // no tiene más ramas
+        right: null, // no tiene más ramas
+    },
+};
+
+const bigTree = {
+    value: 1,
+    left: {
+        value: 5,
+        left: {
+            value: 7,
+            left: {
+                value: 3,
+                left: null,
+                right: null,
+            },
+            right: null,
+        },
+        right: null,
+    },
+    right: {
+        value: 6,
+        left: {
+            value: 5,
+            left: null,
+            right: null,
+        },
+        right: {
+            value: 1,
+            left: null,
+            right: null,
+        },
+    },
+};
+
+function countDecorations(bigTree) {
+    return bigTree !== null
+        ? bigTree.value + countDecorations(bigTree.left) + countDecorations(bigTree.right)
+        : 0;
+}
+
+console.log(countDecorations(tree));
+console.log(countDecorations(bigTree));
